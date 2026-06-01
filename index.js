cbcf7397c6427ccb6529d5575815a8ec7d12d1da
NikodemM <malpa1725@gmail.com>
2026-06-01 09:24:05 +0200

    mandag 01.06

---
+++ index.js
@@ -0,0 +1,4 @@
+buttonELM = document.getElementById("btn")
+buttonELM.addEventListener("click", function () {
+    window.location.href = "file:///C:/Users/MSI/OneDrive%20-%20Akershus%20fylkeskommune/Pulpit/Polsk%20Skriftlig/IT-Final-prosjekte/slots.html";
+})
