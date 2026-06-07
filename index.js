b50c81b860613a742d355fbb8320f1a729d31491
NikodemM <malpa1725@gmail.com>
2026-06-07 22:25:51 +0200

    1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 buttonELM = document.getElementById("btn")
 buttonELM.addEventListener("click", function () {
-    window.location.href = "file:///C:/Users/MSI/OneDrive%20-%20Akershus%20fylkeskommune/Pulpit/Polsk%20Skriftlig/IT-Final-prosjekte/slots.html";
+    window.location.href = "file:///C:/Users/nikod/OneDrive/Dokumenty/GitHub/IT-Final-prosjekte/slots.html";
 })
